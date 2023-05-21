a69b7b91d46c7ab0b94462ed1b723f07c39cdb41
zoey <60343059+zoey250@users.noreply.github.com>
2023-05-22 00:08:44 +0800

    修改入参, 增加extern
    git push太卡了, 直接传了

--- OP/selectivity.cc
+++ OP/selectivity.cc
@@ -3,30 +3,29 @@
 //
 #include "selectivity.h"
 
-double calSimpleSelectivity(int relNum,  const std::vector<std::vector<QL_Condition>>& simpleConditions) {
-    int numConditions = simpleConditions[relNum].size();
+double calSingleRelSelectivity(const std::vector<QL_Condition>& conditions) {
+    int numConditions = conditions.size();
 
     double selectivity = 1.0;
 
     if (numConditions == 1) {
-
         // 创建属性不同值个数变量diff_value，初始化为10
         int diff_values = 10;
 
         // 根据条件操作符计算选择率
-        if (simpleConditions[relNum][0].op == EQ_OP) {
+        if (conditions[0].op == EQ_OP) {
             selectivity = 1.0 / diff_values;
         }
-        if (simpleConditions[relNum][0].op == LT_OP || simpleConditions[relNum][0].op == GT_OP ||
-            simpleConditions[relNum][0].op == LE_OP || simpleConditions[relNum][0].op == GE_OP) {
+        if (conditions[0].op == LT_OP || conditions[0].op == GT_OP ||
+            conditions[0].op == LE_OP || conditions[0].op == GE_OP) {
             selectivity = 1.0 / 3.0;
         }
-        if (simpleConditions[relNum][0].op == NE_OP) {
+        if (conditions[0].op == NE_OP) {
             selectivity = 1.0 - (1.0 / diff_values);
         }
     }
     if (numConditions > 1) {
-        for (const auto& cond : simpleConditions[relNum]) {
+        for (const auto& cond : conditions) {
             // 创建属性不同值个数变量diff_value，初始化为10
             int diff_values = 10;
 
@@ -42,22 +41,23 @@ double calSimpleSelectivity(int relNum,  const std::vector<std::vector<QL_Condit
             }
         }
     }
+
     /*
     if (numConditions >= 1) {
     std::vector<double> selectivities;
 
-    for (const auto& cond : simpleConditions[relNum]) {
+    for (const auto& cond : conditions) {
         // 根据条件操作符计算选择率
-        double conditionSelectivity = 1.0f;
+        double conditionSelectivity = 1.0;
         if (cond.op == EQ_OP) {
             // 获取属性不同值个数
             int diff_values = 10;  // 这里需要根据实际情况获取属性不同值个数
-            conditionSelectivity = 1.0f / diff_values;
+            conditionSelectivity = 1.0 / diff_values;
         } else if (cond.op == LT_OP || cond.op == GT_OP || cond.op == LE_OP || cond.op == GE_OP) {
-            conditionSelectivity = 1.0f / 3.0f;
+            conditionSelectivity = 1.0 / 3.0;
         } else if (cond.op == NE_OP) {
             int diff_values = 10;  // 这里需要根据实际情况获取属性不同值个数
-            conditionSelectivity = 1.0f - (1.0f / diff_values);
+            conditionSelectivity = 1.0 - (1.0 / diff_values);
         }
 
         selectivities.push_back(conditionSelectivity);
@@ -71,6 +71,7 @@ double calSimpleSelectivity(int relNum,  const std::vector<std::vector<QL_Condit
         selectivity *= std::pow(selectivities[i], 1.0f / std::pow(2, i));
     }
 }*/
+
     return selectivity;
 }
 
--- OP/selectivity.h
+++ OP/selectivity.h
@@ -10,8 +10,8 @@
 #include "ql.h"
 #include "ql_iterator.h"
 
-double calSimpleSelectivity(int relNum,  const std::vector<std::vector<QL_Condition>>& simpleConditions);
-double calJoinSelectivity(double leftSelectivity, double rightSelectivity);
+extern double calSingleRelSelectivity(const std::vector<QL_Condition>& conditions);
+extern double calJoinSelectivity(double leftSelectivity, double rightSelectivity);
 
 #endif //SELECTIVITY_H
 
