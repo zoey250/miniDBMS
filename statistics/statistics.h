5d6f3f3dbc418d64614fbe64ed3a141bc0507b0a
e1iaz <ncabhd@hotmail.com>
2023-06-02 08:22:04 +0800

    Add statistics.

---
+++ statistics/statistics.h
@@ -0,0 +1,14 @@
+//
+// Created by elias on 6/1/23.
+//
+
+#ifndef MICRODBMS_STATISTICS_H
+#define MICRODBMS_STATISTICS_H
+
+static char *REL_STATISTICS = "relstatistics";
+static char *REL_NAME = "relname";
+static char *ATTR_NAME = "attrname";
+static char *BUCKET_NUM = "bucketnum";
+static char *VALUE = "value";
+
+#endif //MICRODBMS_STATISTICS_H
